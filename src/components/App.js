f87073f469b213fee263ba700428662656fd13c7
Abhinaba <abhi.roychoudhury.8@gmail.com>
2024-11-21 13:25:52 +0530

    3rd

--- src/components/App.js
+++ src/components/App.js
@@ -39,7 +39,7 @@ const App = () => {
 
  return (
    <div>
-     <div>Data Fetched from API</div>
+     <h1>Data Fetched from API</h1>
      <pre>{JSON.stringify(data, null, 2)}</pre>
    </div>
  );
